1533862ba7f2b4bd44cfb2c4f64f4d0cd87de4b1
VictorRandom <victormbalduino@hotmail.com>
2022-07-18 13:54:32 -0300

    state and effect practice

---
+++ src/WindowTracker.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react"
+
+export default function WindowTracker() {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener("resize", function () {
+            setWindowWidth(window.innerWidth)
+        })
+    }, [])
+
+    return (
+        <h1>Window width: {windowWidth}</h1>
+    )
+}
